e5bd0e3e2d9eec02599172341c2d3b10d9a1f379
harsh vardhan upadhyay <harshvardhan.upadhyay@codinova.com>
2021-07-22 16:19:37 +0530

    phusing code into gitlab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Card1 from './componenets/Card1'
+import Card2 from './componenets/Card2'
+import {data} from './componenets/data'
 
-function App() {
+const App = () => {
+
+  const [cartItem, setCartItem] = useState([])
+  const onAddToCart = (product)=>{
+    const exist = cartItem.find((x)=>x.id===product.id)
+    if(exist){
+      setCartItem(
+        cartItem.map((x)=>x.id === product.id ? {...exist, qty:exist.qty+1}: x)
+      )
+    }else{
+      setCartItem([...cartItem, {...product, qty:1}])
+    }
+  }
+  const onRemoveToCart = (product)=>{
+    const exist = cartItem.find(x=>x.id===product.id)
+    if(exist.qty===1){
+      setCartItem(cartItem.filter(x=>x.id !== product.id))
+    }else{
+      setCartItem(
+        cartItem.map(x=>x.id === product.id ? {...exist, qty:exist.qty-1}: x)
+      )
+    }
+  }
+  const deleteCart = (product)=>{
+    const exist = cartItem.find(x=>x.id===product.id)
+    if(exist){
+      setCartItem(cartItem.filter(x=>x.id !== product.id))
+    }
+  }
+
+  const cancelCart = ()=>{
+    setCartItem([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Card1 cancelCart={cancelCart} cartItem={cartItem} onAddToCart={onAddToCart} onRemoveToCart={onRemoveToCart} deleteCart={deleteCart} />
+    <Card2 onAddToCart={onAddToCart} data={data} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
